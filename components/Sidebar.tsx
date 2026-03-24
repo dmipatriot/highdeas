8b7a664985b3b2ee3c32d9b90c715a4b17703d9b
Kenny <kenny.thorson@gmail.com>
2026-03-23 19:12:48 -0500

    remove login

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -47,16 +47,6 @@ export default function Sidebar() {
           );
         })}
       </nav>
-
-      {/* Log out */}
-      <div className="border-t border-[#00FF9C]/10 py-2">
-        <button className="flex items-center gap-3 w-full px-4 py-2.5 text-xs tracking-widest text-[#ababab] opacity-70 hover:bg-[#00FF9C]/5 hover:opacity-100 transition-colors border-l-4 border-transparent">
-          <span className="material-symbols-outlined text-base leading-none">
-            logout
-          </span>
-          LOG_OUT
-        </button>
-      </div>
     </aside>
   );
 }
